842376232b53663c87d83254ba7ae9df9c0823fe
Quiros Benjamin <bq170921@gmail.com>
2025-04-26 18:05:34 -0300

    realice el ejercicio 2

--- js/02-ejercicio.js
+++ js/02-ejercicio.js
@@ -11,7 +11,16 @@ do{
     let anadidas = prompt("ingresa el nombre de una ciudad")
     ciudades.unshift(anadidas);
 }while(confirm ("desea realizar otra operacion"));
+document.writeln("el arreglo tiene ", ciudades.length , " elementos")
+document.writeln("<ul><li>el primer elemento es ",ciudades[0], "</li>")
+document.writeln("<li>el segundo elemento es ",ciudades[1], "</li>")
+document.writeln("<li>el tercer elemento es ",ciudades[2], "</li>")
+document.writeln("<li>el ultimo elemento agregado por el usuario es ",ciudades[ciudades.length-2], "</li>")
+document.writeln("<li>el ultimo elemento es ",ciudades[ciudades.length-1], "</li>")
+//agregar el cambio de el elemento 3
+ciudades.splice(1,0,"barcelona")
+document.writeln("<h1>Arreglo de ciudades</h1>")
     document.writeln("<ul>")
     for(let lista=0;lista <=ciudades.length-1;lista++){
-        document.writeln("<li>",ciudades[lista],"</li>")
+        document.writeln("<li> Elemento: ",ciudades[lista],"</li>")
     }
