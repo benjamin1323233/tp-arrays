45f7b56a83ef9a0f2fda66c59342a9453e57a651
Quiros Benjamin <bq170921@gmail.com>
2025-04-28 20:56:43 -0300

    realice el ejercicio 7

--- js/03-ejercicio.js
+++ js/03-ejercicio.js
@@ -3,3 +3,18 @@
  para cada uno de los lanzamientos de los dados. Sumar el resultado de lanzar dos dados 
  y anotar en un array el número de apariciones de dicha suma, repitiendo 50 veces esta operación.*/
 
+const resultados = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+for (let repeticiones = 1; repeticiones < 51; repeticiones++) {
+  let dadouno = Math.floor(Math.random() * (6 - 1) + 1);
+  let dadodos =Math.floor(Math.random() * (6 - 1) + 1);
+  suma = dadouno+dadodos-2
+  resultados[suma]+= 1
+}
+document.writeln(`<table><tbody>`);
+document.writeln(` <tr> `);
+document.writeln("<td>resultados</td><td>apariciones</td>");
+document.writeln(` </tr>  `);
+for (let resu = 2; resu <= 12; resu++){ document.writeln(` <tr> `)
+document.writeln("<td>", resu, "</td>", "<td>", resultados[resu - 2], "</td>");
+document.writeln(` </tr>  `);}
+document.writeln(`</tbody></table>`);
